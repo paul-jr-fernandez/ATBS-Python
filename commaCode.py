963cfd67b271ee5a8788c46fcc02cbd407587446
Paul Jr Fernandez <paul.jr.fernandez@gmail.com>
2018-01-18 01:13:36 +1000

    v1.0 Ch4 - Lists - Intial Release

---
+++ commaCode.py
@@ -0,0 +1,27 @@
+# Chapter 4: Lists - Comma Code
+# Author: Paul Jr Fernandez
+# Changelog:
+# 18 Jan 2017 v1.0: Initial Release
+
+# Function splitList that converts the input list into a comma/and separated string
+def splitList(mirrorList):
+    sentenceString = '' # Intialize empty string
+    listLength = len(mirrorList)
+    for i in range(listLength):
+        if i == (listLength - 2): # Handle second last list item
+            sentenceString += mirrorList[i] + ' and '
+        elif i == (listLength - 1): # Handle last list item
+            sentenceString += mirrorList[i]
+        else: # Handle all other list items
+            sentenceString += mirrorList[i] + ', '
+    return sentenceString
+
+# Main
+inputString = input('Enter the list items of your list(Separate list items by SPACE. To stop press ENTER):\n')
+inputList = [ str(x) for x in inputString.split(' ')] # Split input string on basis of spaces and feed into a list
+while '' in inputList: # Remove extra spaces provided at input
+    inputList.remove('')
+print(' The List that has been provided:')
+print(inputList)
+outputString = splitList(inputList)
+print(' The comma coded output string:\n' + outputString)
